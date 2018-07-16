86e1290071faa68c2616e45bd4214f35bb9c084d
Compro-Prasad <comproprasad@gmail.com>
2018-07-16 15:08:48 +0530

    Added isPrime(Int) -> Boolean

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,22 @@
-object Main extends App {
-  println("Hello, World!")
+import Math._
+
+object Test {
+  def isPrime(n : Int): Boolean = {
+    lazy val x = Math.sqrt(n).toInt
+    if (n < 2)
+      false
+    else if (n == 2)
+      true
+    else
+      (3 to x by 2).exists(i => n % i == 0)
+  }
+
+  def main(args : Array[String]) {
+    val n = 10
+    var i = 0
+    while (i < n) {
+      println(s"$i = ${isPrime(i)}")
+      i += 1
+    }
+  }
 }
